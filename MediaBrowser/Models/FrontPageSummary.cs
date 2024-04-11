df24f7d989b869fd03d5e83b89d7c226285be868
Cameron Battagler <cameronb@hey.com>
2024-04-10 22:30:01 -0500

    Added scrolling front page Added About Page

--- MediaBrowser/Models/FrontPageSummary.cs
+++ MediaBrowser/Models/FrontPageSummary.cs
@@ -6,7 +6,7 @@
         public List<DirectorWithCount> PopularDirectors { get; set; }
         public List<GenreWithCount> PopularGenres { get; set; }
         public List<Movie> MostRecentMovies { get; set; }
-        public Movie? RandomMovie { get; set; }
+        public List<Movie> RandomMovies { get; set; }
         public List<MovieWithRating> TopRatedMovies { get; set; }
         public List<MovieWithRating> WorstRatedMovies { get; set; }
     }
--- MediaBrowser/Pages/Index.cshtml.cs
+++ MediaBrowser/Pages/Index.cshtml.cs
@@ -25,7 +25,7 @@ namespace MediaBrowser.Pages
                 MostRecentMovies = await _context.Movies.OrderByDescending(m => m.ReleaseDate).Take(5).ToListAsync(),
                 TopRatedMovies = await _context.Movies.Include(c => c.Criticreviews).AsNoTracking().Select(a => new MovieWithRating { Movie = a, Rating = (a.PopularityScore + a.Criticreviews.First().CriticScore)/2 }).OrderByDescending(m => m.Rating).Take(5).ToListAsync(),
                 WorstRatedMovies = await _context.Movies.Include(c => c.Criticreviews).AsNoTracking().Select(a => new MovieWithRating { Movie = a, Rating = (a.PopularityScore + a.Criticreviews.First().CriticScore) / 2 }).OrderBy(m => m.Rating).Take(5).ToListAsync(),
-                RandomMovie = await _context.Movies.FirstOrDefaultAsync(m => m.MovieId == randMovieId),
+                RandomMovies = await _context.Movies.OrderBy(r => Guid.NewGuid()).Take(3).ToListAsync(),
                 PopularGenres = await _context.Genres.Select(a => new GenreWithCount { Genre = a, MovieCount = a.Movies.Count() }).OrderByDescending(m => m.MovieCount).Take(5).ToListAsync(),
                 PopularDirectors = await _context.Directors.Select(a => new DirectorWithCount { Director = a, MovieCount = a.Movies.Count() }).OrderByDescending(m => m.MovieCount).Take(5).ToListAsync(),
                 PopularActors = await _context.Actors.Select(a => new ActorWithCount { Actor = a, MovieCount = a.Movies.Count() }).OrderByDescending(m => m.MovieCount).Take(5).ToListAsync()
